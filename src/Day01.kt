0a27c936f3b2ea40ef3bb2f4b3eb4b0c9d8e6685
W. Michael Shirk <wmichaelshirk@gmail.com>
2022-12-01 10:15:44 -0600

    feat: day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,28 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val elfRations = input
+            .joinToString(",")
+            .split(",,")
+            .map {
+                it.split(',').sumOf { n -> n.toInt() }
+            }
+        return elfRations.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elfRations = input
+            .joinToString(",")
+            .split(",,")
+            .map {
+                it.split(',').sumOf { n -> n.toInt() }
+            }
+        return elfRations.sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
